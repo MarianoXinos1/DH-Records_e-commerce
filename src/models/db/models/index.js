de0af149dd024622c89e23d5e24e010c7beadadb
MarianoXinos1 <xinosmariano@gmail.com>
2024-08-18 14:57:16 -0300

    Fix3: Vercel

--- src/models/db/models/index.js
+++ src/models/db/models/index.js
@@ -1,34 +1,23 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
 let sequelize;
-if (config && config.use_env_variable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else if (config) {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
 } else {
-  throw new Error('La configuración de la base de datos no está definida.');
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .sort() 
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
